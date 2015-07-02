65a360a5f86020a947032ef20c5257e2fcd0f63a
marcoviappiani <marco.viappiani@gmail.com>
2015-07-02 16:43:47 -0700

    passed tests for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,44 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+    //list = node;
+    if (list.tail!==null){
+      list.tail.next = node;
+    }
+    // var oldTail = list.tail;
+    list.tail = node;
+    if (list.head===null){ // this may need to be different, (eg single-node-list-head is removed, and then new tail is added)
+      list.head = list.tail;
+    }
+    
+    
+    // list.head = node;
   };
 
   list.removeHead = function(){
+    var headValue = list.head.value;
+    list.head = list.head.next;
+    if (list.head===null){
+      list.tail = null;
+    }
+    return headValue;
   };
 
   list.contains = function(target){
+    var targetFound = false;
+    var searchForTarget = function(node) {
+      if(node.value === target) {
+        targetFound = true;
+      }
+      if(node.next === null) {
+        return;
+      }
+      searchForTarget(node.next);
+
+    };
+    searchForTarget(list.head);
+    return targetFound;
   };
 
   return list;
@@ -21,7 +53,7 @@ var Node = function(value){
   node.value = value;
   node.next = null;
 
-  return node;
+  return node;  
 };
 
 /*
