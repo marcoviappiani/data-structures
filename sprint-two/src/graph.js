4cff8b5f452c64a421fcc95cb4f5202a191e8460
marcoviappiani <marco.viappiani@gmail.com>
2015-07-03 12:29:09 -0700

    passed graph and tree

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,62 @@
 
 
 var Graph = function(){
+	// var obj = Object.create(Graph.prototype);
+
+	this.nodes = {};
+	this.edges = {};
+
+	// return obj;
 };
 
 Graph.prototype.addNode = function(node){
-};
-
-Graph.prototype.contains = function(node){
+	// this = newNode;
+	this.nodes[node] = node;
+	// newNode.value = value;
 };
 
 Graph.prototype.removeNode = function(node){
+	if(this.nodes[node] === node) {
+		this.nodes[node] = null;
+	}
 };
 
-Graph.prototype.hasEdge = function(fromNode, toNode){
+Graph.prototype.contains = function(target){
+	if(this.nodes[target] === target) {
+		return true;
+	}
+	return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+	var edgeName = fromNode + toNode;
+	var reverseEdge = toNode + fromNode;
+	this.edges[edgeName] = edgeName;
+	this.edges[reverseEdge] = reverseEdge;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+	var edgeName = fromNode + toNode;
+	var reverseEdge = toNode +fromNode;
+	if(this.edges[edgeName] === edgeName) {
+		this.edges[edgeName] = null;
+		this.edges[reverseEdge] = null;
+	}
 };
 
-Graph.prototype.forEachNode = function(cb){
+Graph.prototype.hasEdge = function(fromNode, toNode){
+	var edgeName = fromNode + toNode;
+	var reverseEdge = toNode + fromNode;
+	if(this.edges[edgeName] === edgeName) {
+		return true;
+	}
+	return false;
+};
+
+Graph.prototype.forEachNode = function(callback){
+	for (var node in this.nodes){
+		callback(node);
+	}
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
 var Tree = function(value){
-  var newTree = {};
-  newTree.value = value;
+  var newTree = Object.create(treeMethods);
+  
+  newTree.value = value; // This is the value of the tree instance 
 
   // your code here
-  newTree.children = null;  // fix me
+
+  newTree.children = [];  
 
   return newTree;
 };
@@ -15,14 +17,33 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+	var newChild = Tree(value);
+	this.children.push(newChild);
 };
 
 treeMethods.contains = function(target){
+	var targetFound = false;
+
+	var searchNode = function(node){
+		if(node.value === target){
+			targetFound = true;
+			return;
+		}
+		var nodeChildren = node.children;
+		for(var i = 0; i<nodeChildren.length; i++){
+			searchNode(nodeChildren[i]);
+		}
+	};
+
+	searchNode(this);
+
+	return targetFound;
 
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addChild: Constant
+ contains: Linear
  */
